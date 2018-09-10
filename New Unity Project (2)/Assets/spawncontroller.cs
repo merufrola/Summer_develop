88a0f46e81977e6eb05bdb09529b659fad70132f
merufrola <merufrola@gmail.com>
2018-09-10 13:53:02 +0900

    敵AI作成中

---
+++ New Unity Project (2)/Assets/spawncontroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawncontroller : MonoBehaviour {
+public GameObject[] type;
+public int[,] Spawner  = new int[3,3];
+public int[,] Goal = new int[4,3];
+int randTYPE;
+int randSPN;
+int randGOAL;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+// randTYPE = Random.Range(0,4);
+// randSPN = Random.Range(0,2);
+// randGOAL = Random.Range(0.3);
+// Instantiate (type[randTYPE], new Vector3(Spawner[randSPN,1],Spawner[randSPN,2],Spawner[randSPN,3]),Quaternion.identity);
+//
+
+	}
+}
