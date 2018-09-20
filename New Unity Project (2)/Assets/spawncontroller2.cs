f0edbf918b8e3ebc01b18b136cca327ad41028e4
merufrola <merufrola@gmail.com>
2018-09-20 18:05:08 +0900

    いろいろ追加したやつ

---
+++ New Unity Project (2)/Assets/spawncontroller2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawncontroller2 : MonoBehaviour {
+	public GameObject target;
+	public float[] Spawner  = new float[3];
+
+
+
+	int randSPN;
+
+	int randTime;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("spawner",2f,10f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void spawner(){
+
+		for(int i = 3; i>0; --i){
+			randSPN = Random.Range(0,3);
+			randTime = Random.Range(0,3);
+		Instantiate (target, new Vector3(Spawner[randSPN],0,-0.8f + Spawner[randTime]),Quaternion.identity);
+		Debug.Log("drop");
+		}
+
+
+	}
+}
